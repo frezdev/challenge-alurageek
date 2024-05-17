eba3eef9df5f05ed5537caf40789af836c975ba5
frezdev <carlos.andres.ff2912@gmail.com>
2024-05-16 22:26:29 -0500

    Add vercel config for api

---
+++ api/index.js
@@ -0,0 +1,20 @@
+const jsonServer = require('json-server');
+
+const server = jsonServer.create();
+const router = jsonServer.router('db.json');
+const middlewares = jsonServer.defaults();
+
+const PORT = process.env.PORT ?? 3001;
+
+server.use(middlewares);
+// Add this before server.use(router)
+
+server.use(router)
+
+
+server.listen(PORT, () => {
+    console.log('JSON Server is running')
+})
+
+// Export the Server API
+module.exports = server
