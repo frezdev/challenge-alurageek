117c74ccf3f9cab0589b9ab942bcc12afc5e5f8d
frezdev <carlos.andres.ff2912@gmail.com>
2024-05-18 17:18:14 -0500

    Add validations to the forn inputs

--- src/products/createProduct.js
+++ src/products/createProduct.js
@@ -1,19 +1,27 @@
 import { Products } from "../services/products.js";
+import { validateInput } from "../utils/form-validate/validateInput.js";
 
 const $ = document;
 
 const form = $.querySelector('[data-form]');
+const inputsList = form.querySelectorAll('input,select');
+
+inputsList.forEach(input => {
+  input.addEventListener('input', () => validateInput(input));
+  input.addEventListener('invalid', (e) => e.preventDefault());
+});
 
 export default function createProduct () {
   form.addEventListener('submit', async (e) => {
-    e.preventDefault()
+    e.preventDefault();
+
     const formData = new FormData(form);
     const product = Object.fromEntries(formData);
 
-    console.log({product});
-    const [error, data] = await Products.create(product)
+    const [error, data] = await Products.create(product);
 
-    console.log({error, data})
+    if (error) {
+      console.error(error);
+    }
   });
-
 }
